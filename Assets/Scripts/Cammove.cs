c9c9c6e34de2fe8633a595a0a6de4ee6cecb56cf
vijit101 <40598861+vijit101@users.noreply.github.com>
2018-07-24 19:18:50 +0530

    First Game commit
    Can be a great game

---
+++ Assets/Scripts/Cammove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class Cammove : MonoBehaviour {
+    [SerializeField] GameObject myplay;
+    Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - myplay.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = myplay.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour {
+    Rigidbody rgbd;
+    private void Awake()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        moveleft();
+        moveright();
+        jump();
+    }
+    public void moveleft()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rgbd.AddForce(new Vector3(-15, 0, 0));
+        }
+    }
+    public void moveright()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rgbd.AddForce(new Vector3(15, 0, 0));
+        }
+    }
+    public void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rgbd.AddForce(new Vector3(0,10,0),ForceMode.Impulse);
+        }
+    }
+}
